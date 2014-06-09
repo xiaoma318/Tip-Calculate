e8c50bd1b28944b4ac3ca75ed63b37bc3eb2caa4
Cheng <chengma@yahoo-inc.com>
2014-06-08 17:43:10 -0700

    add more func

--- gen/com/cm/TipApp/R.java
+++ gen/com/cm/TipApp/R.java
@@ -10,6 +10,7 @@ package com.cm.TipApp;
 public final class R {
     public static final class array {
         public static final int percent=0x7f050000;
+        public static final int person=0x7f050001;
     }
     public static final class attr {
     }
@@ -17,13 +18,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int TextView01=0x7f06000c;
+        public static final int TextView02=0x7f06000b;
+        public static final int TextView03=0x7f06000d;
+        public static final int TextView04=0x7f06000e;
         public static final int button1=0x7f060001;
         public static final int button2=0x7f060002;
         public static final int button3=0x7f060003;
         public static final int inputText=0x7f060000;
-        public static final int spinner1=0x7f060006;
-        public static final int textView1=0x7f060004;
-        public static final int textView2=0x7f060005;
+        public static final int spinner1=0x7f060004;
+        public static final int spinner2=0x7f06000a;
+        public static final int textView1=0x7f060005;
+        public static final int textView2=0x7f060006;
+        public static final int textView3=0x7f060008;
+        public static final int textView4=0x7f060007;
+        public static final int textView5=0x7f060009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/cm/TipApp/MyActivity.java
+++ src/com/cm/TipApp/MyActivity.java
@@ -13,9 +13,12 @@ import android.widget.TextView;
 
 public class MyActivity extends Activity {
 	Button b1, b2, b3;
-	TextView input, ret;
-	Spinner s;
+	TextView input, ret, total, each, eTip;
+	Spinner more, split;
 	DecimalFormat df = new DecimalFormat("#.##");
+	int numOfPerson = 1;
+	double sum = 0;
+
 	/**
 	 * Called when the activity is first created.
 	 */
@@ -28,8 +31,12 @@ public class MyActivity extends Activity {
 		b3 = (Button) findViewById(R.id.button3);
 		input = (TextView) findViewById(R.id.inputText);
 		ret = (TextView) findViewById(R.id.textView2);
-		s = (Spinner) findViewById(R.id.spinner1);
-		
+		total = (TextView) findViewById(R.id.textView4);
+		each = (TextView) findViewById(R.id.TextView02);
+		eTip = (TextView) findViewById(R.id.TextView04);
+		more = (Spinner) findViewById(R.id.spinner1);
+		split = (Spinner) findViewById(R.id.spinner2);
+
 		setListener();
 	}
 
@@ -38,8 +45,15 @@ public class MyActivity extends Activity {
 		b1.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				double money = Double.parseDouble(input.getText().toString());
-				ret.setText(df.format(money * 0.1) + "");
+				if (!input.getText().toString().isEmpty()) {
+					double money = Double.parseDouble(input.getText()
+							.toString());
+					sum = money * 1.1;
+					ret.setText(df.format(money * 0.1) + "");
+					total.setText(df.format(money * 1.1) + "");
+					each.setText(df.format(money * 1.1 / numOfPerson));
+					eTip.setText(df.format(money * 0.1 / numOfPerson));
+				}
 			}
 
 		});
@@ -47,8 +61,15 @@ public class MyActivity extends Activity {
 		b2.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				double money = Double.parseDouble(input.getText().toString());
-				ret.setText(df.format(money * 0.15) + "");
+				if (!input.getText().toString().isEmpty()) {
+					double money = Double.parseDouble(input.getText()
+							.toString());
+					sum = money * 1.15;
+					ret.setText(df.format(money * 0.15) + "");
+					total.setText(df.format(money * 1.15) + "");
+					each.setText(df.format(money * 1.15 / numOfPerson));
+					eTip.setText(df.format(money * 0.15 / numOfPerson));
+				}
 			}
 
 		});
@@ -56,29 +77,60 @@ public class MyActivity extends Activity {
 		b3.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				double money = Double.parseDouble(input.getText().toString());
-				ret.setText(df.format(money * 0.2) + "");
+				if (!input.getText().toString().isEmpty()) {
+					double money = Double.parseDouble(input.getText()
+							.toString());
+					sum = money * 1.2;
+					ret.setText(df.format(money * 0.2) + "");
+					total.setText(df.format(money * 1.2) + "");
+					each.setText(df.format(money * 1.2 / numOfPerson));
+					eTip.setText(df.format(money * 0.2 / numOfPerson));
+				}
 			}
 
 		});
-		
-		
-		s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+		more.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
 			@Override
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
 					int arg2, long arg3) {
-				double money = Double.parseDouble(input.getText().toString());
-				double percent = Double.parseDouble(s.getItemAtPosition(arg2).toString().split("%")[0])/100;
-				ret.setText(df.format(money * percent));
+				if (!input.getText().toString().isEmpty()) {
+					double money = Double.parseDouble(input.getText()
+							.toString());
+					double percent = Double.parseDouble(more
+							.getItemAtPosition(arg2).toString().split("%")[0]) / 100;
+					sum = money * (1 + percent);
+					ret.setText(df.format(money * percent));
+					total.setText(df.format(sum) + "");
+					each.setText(df.format(sum / numOfPerson));
+					eTip.setText(df.format(money * percent / numOfPerson));
+				}
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
+		});
+
+		split.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				numOfPerson = Integer.parseInt(split.getItemAtPosition(arg2)
+						.toString());
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
 		});
 
 	}
